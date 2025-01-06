695f4021bae0f0f49700540edf8860c7257c2eea
Md Noorullah Raza <mnraza94@gmail.com>
2025-01-06 13:22:33 +0530

    feat: add styles to Main component and improve font usage

--- todo_app/src/app/index.js
+++ todo_app/src/app/index.js
@@ -1,14 +1,14 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import { useFonts } from 'expo-font'
 
 const Main = () => {
 
-    const {loaded} = useFonts(
-        {
-            SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf')
-        }
-    );
+  const { loaded } = useFonts(
+    {
+      SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf')
+    }
+  );
 
   return (
     <View>
@@ -16,5 +16,15 @@ const Main = () => {
     </View>
   )
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontFamily: 'SpaceMono'
+
+  }
+})
 
 export default Main
