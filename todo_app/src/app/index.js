191457503565d67e6ede3ec04aad2bf0c88af764
Md Noorullah Raza <mnraza94@gmail.com>
2025-01-06 13:19:24 +0530

    feat: add custom font loading with useFonts in Main component

--- todo_app/src/app/index.js
+++ todo_app/src/app/index.js
@@ -1,7 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useFonts } from 'expo-font'
 
 const Main = () => {
+
+    const {loaded} = useFonts(
+        {
+            SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf')
+        }
+    );
+
   return (
     <View>
       <Text>Index</Text>
