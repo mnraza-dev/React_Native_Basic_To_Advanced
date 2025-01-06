8e852de79225f96857ad988a1c73fa8ef2876038
Md Noorullah Raza <mnraza94@gmail.com>
2025-01-06 23:31:16 +0530

    feat: enhance Home component with empty state UI; add image and styled message when no todos are available

--- todo_app/src/app/home.jsx
+++ todo_app/src/app/home.jsx
@@ -1,9 +1,10 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native'
+import { FlatList, Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
 import { useSelector } from 'react-redux';
 import Header from "./../components/Header"
 import AddButton from '../components/AddButton';
+import { screenWidth, screenHeight } from "../utils/contants";
 const Home = () => {
   const data = useSelector(state => state.todo.data);
   console.log(data);
@@ -18,17 +19,31 @@ const Home = () => {
   return (
     <View style={styles.container}>
       <Header title="Todo App" isBackButton={false} />
-      <Text>Home 2 screen </Text>
+
 
       <FlatList
-        data={data}
+        data={[]}
         initialNumToRender={10}
         key={item => item.id}
         keyExtractor={item => item.id}
         renderItem={renderTodoItem}
         windowSize={10}
         showsVerticalScrollIndicator={false}
-        ListEmptyComponent={<View><Text>Click on plus button to add yours todo tasks!</Text></View>}
+        ListEmptyComponent={<View style={styles.emptyContainer}>
+          <Image
+            source={require('./../assets/images/add.png')}
+            style={styles.emptyImage} />
+          <Text style={{
+            textAlign: 'center',
+            width: '100%',
+            fontSize: 20,
+            fontWeight: 'bold',
+            color: '#f0f0f0'
+          }}>
+
+            Click on plus button to add yours todo tasks!
+          </Text>
+        </View>}
       />
       <AddButton />
 
@@ -44,5 +59,16 @@ const styles = StyleSheet.create({
     // backgroundColor: '#fff',
     // position: 'relative',
 
+  },
+  emptyImage: {
+    height: screenWidth * 0.5,
+    width: screenHeight * 0.2,
+    objectFit: 'contain'
+  },
+  emptyContainer: {
+    height: screenHeight * 0.8,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 })
