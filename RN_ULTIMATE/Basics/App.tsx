533966ea3e016a58182e7ce2f5fca79129140128
Md Noorullah Raza <mnraza94@gmail.com>
2025-10-20 16:55:35 +0530

    update App component to display a personalized greeting using Text component

--- RN_ULTIMATE/Basics/App.tsx
+++ RN_ULTIMATE/Basics/App.tsx
@@ -2,9 +2,10 @@ import { View, Text } from 'react-native'
 import React from 'react'
 
 const App = () => {
+  const name ="MN Raza"
   return (
-    <View>
-      <Text>Hello</Text>
+    <View style={{ backgroundColor: 'green' }}>
+      <Text style={{ fontSize: 44,padding:20, textAlign: 'center', color: '#fff' }}>Hello, {name}</Text>
     </View>
   )
 }
