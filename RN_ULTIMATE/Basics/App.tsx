c8f23b06fce4ad45ba97991a6ea1c0149a8e4cad
Md Noorullah Raza <mnraza94@gmail.com>
2025-10-20 17:15:53 +0530

    feat: update App component to handle fruit selection and modify README to include Buttons section

--- RN_ULTIMATE/Basics/App.tsx
+++ RN_ULTIMATE/Basics/App.tsx
@@ -1,13 +1,21 @@
-import { View, Text, Button, Alert } from 'react-native'
+import { View, Button, Alert } from 'react-native'
 import React from 'react'
 import UserData from './components/UserData'
 
 const App = () => {
+
+  const fruit =(fruit)=>{
+    console.log('function called',fruit);
+    
+  }
   const name ="MN Raza"
   return (
     <View style={{ backgroundColor: 'green' }}>
       <UserData/>
-      <Button title='Tap here' onPress={()=>Alert.alert("Hi There")}/>
+      <Button  title='Tap here' 
+      // onPress={()=>Alert.alert("Hi There")}
+      onPress={()=>fruit('mango')}
+      />
     </View>
   )
 }
--- RN_ULTIMATE/Basics/README.md
+++ RN_ULTIMATE/Basics/README.md
@@ -4,4 +4,5 @@
 
 
 ## Components in React Native
+## Buttons and onPress event in React Native
 ## Lists in React Native
