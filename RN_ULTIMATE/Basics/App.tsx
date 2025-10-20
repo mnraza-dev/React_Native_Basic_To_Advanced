36c84432ad4516fe7b48cecade8f737ae4dfae2b
Md Noorullah Raza <mnraza94@gmail.com>
2025-10-20 17:29:38 +0530

    feat: refactor App component to include name input and greeting functionality

--- RN_ULTIMATE/Basics/App.tsx
+++ RN_ULTIMATE/Basics/App.tsx
@@ -1,23 +1,47 @@
-import { View, Button, Alert } from 'react-native'
-import React from 'react'
-import UserData from './components/UserData'
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [name, setName] = useState('Your Name')
 
-  const fruit =(fruit)=>{
-    console.log('function called',fruit);
-    
+  const greet = (newName) => {
+    setName(newName)
   }
-  const name ="MN Raza"
+
   return (
-    <View style={{ backgroundColor: 'green' }}>
-      <UserData/>
-      <Button  title='Tap here' 
-      // onPress={()=>Alert.alert("Hi There")}
-      onPress={()=>fruit('mango')}
+    <View style={styles.container}>
+      <Text style={styles.heading}>{name}</Text>
+      <TextInput
+        onChangeText={(text) => setName(text)}
+        value={name}
+        placeholder="Enter Your Name"
+        style={styles.textInput}
       />
+
+      <Button color={'green'} title="Update Name" onPress={() => greet('MN Raza')} />
     </View>
   )
 }
 
 export default App
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+  },
+  heading: {
+    textAlign: 'center',
+    fontSize: 44,
+    marginBottom: 20,
+  },
+  textInput: {
+    fontSize: 24,
+    padding: 12,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 10,
+    marginBottom: 20,
+  },
+})
